9e5b91af179295042cadddbb849cbeffa7b1f742
Jeonghwan Yoon <home@192-168-1-110.tpgi.com.au>
2021-07-30 21:37:24 +1000

    editing start

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
+// import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Step 2: Define your component
 const IndexPage = () => {
